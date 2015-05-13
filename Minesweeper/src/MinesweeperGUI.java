6e36a5aec1e7a60856232f84d2a63c1d962b5f5f
chrislee777 <gunwoo777@gmail.com>
2015-05-12 21:23:20 -0500

    Gui Menu
    Added menu

--- Minesweeper/src/MinesweeperGUI.java
+++ Minesweeper/src/MinesweeperGUI.java
@@ -1,11 +1,31 @@
 import java.awt.EventQueue;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JOptionPane;
+
+import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JRadioButtonMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowEvent;
 
 
 public class MinesweeperGUI {
 
-	private JFrame frame;
+	private JFrame frmMinesweeper;
+	
+	private int width;
+	private int height;
+	private int mines;
 
 	/**
 	 * Launch the application.
@@ -15,7 +35,7 @@ public class MinesweeperGUI {
 			public void run() {
 				try {
 					MinesweeperGUI window = new MinesweeperGUI();
-					window.frame.setVisible(true);
+					window.frmMinesweeper.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -27,6 +47,9 @@ public class MinesweeperGUI {
 	 * Create the application.
 	 */
 	public MinesweeperGUI() {
+		width = 9;
+		height = 9;
+		mines = 10;
 		initialize();
 	}
 
@@ -34,9 +57,100 @@ public class MinesweeperGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmMinesweeper = new JFrame();
+		frmMinesweeper.setTitle("Minesweeper");
+		frmMinesweeper.setBounds(100, 100, 700, 454);
+		frmMinesweeper.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmMinesweeper.setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Game");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New");
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				restart();
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmStatistics = new JMenuItem("Statistics");
+		mntmStatistics.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mnNewMenu.add(mntmStatistics);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Exit");
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmMinesweeper.dispatchEvent(new WindowEvent(frmMinesweeper, WindowEvent.WINDOW_CLOSING));
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenu mnMode = new JMenu("Mode");
+		menuBar.add(mnMode);
+		
+		ButtonGroup group = new ButtonGroup();
+		
+		JRadioButtonMenuItem rdbtnmntmBeginnerxMines = new JRadioButtonMenuItem("Beginner              9x9,10 Mines");
+		rdbtnmntmBeginnerxMines.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				width = 9;
+				height = 9;
+				mines = 10;
+			}
+		});
+		rdbtnmntmBeginnerxMines.setSelected(true);
+		group.add(rdbtnmntmBeginnerxMines);
+		mnMode.add(rdbtnmntmBeginnerxMines);
+		
+		JRadioButtonMenuItem rdbtnmntmIntermediatex = new JRadioButtonMenuItem("Intermediate       16x16, 40 mines");
+		rdbtnmntmIntermediatex.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				width = 16;
+				height = 16;
+				mines = 40;
+			}
+		});
+		group.add(rdbtnmntmIntermediatex);
+		mnMode.add(rdbtnmntmIntermediatex);
+		
+		JRadioButtonMenuItem rdbtnmntmAdvancedx = new JRadioButtonMenuItem("Advanced            16x30, 99 Mines");
+		rdbtnmntmAdvancedx.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				width = 30;
+				height = 16;
+				mines = 99;
+			}
+		});
+		group.add(rdbtnmntmAdvancedx);
+		mnMode.add(rdbtnmntmAdvancedx);
+		
+		
+		
+		
+		
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mntmAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Made by Chris Lee, Jason Lu, Derek Chen");
+			}
+		});
+		mnHelp.add(mntmAbout);
+		frmMinesweeper.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 	}
-
+	
+	public void restart(){
+		
+	
+	}
 }
